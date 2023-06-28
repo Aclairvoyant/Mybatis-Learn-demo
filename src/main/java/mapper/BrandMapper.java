858b990ea174f897c2018706ccd7f7407d1a57bb
sjd <shenjiadong1010@gmail.com>
2023-06-28 17:22:12 +0800

    第二次提交Mybatis学习过程

--- src/main/java/mapper/BrandMapper.java
+++ src/main/java/mapper/BrandMapper.java
@@ -4,11 +4,40 @@ import org.apache.ibatis.annotations.Param;
 import pojo.Brand;
 
 import java.util.List;
+import java.util.Map;
 
 public interface BrandMapper {
     List<Brand> selectAll();
 
     Brand selectById(int id);
 
-    List<Brand> selectByCondition(@Param("status") int status, @Param("companyName") String companyName, @Param("brandName") String brandName);
+    //List<Brand> selectByCondition(@Param("status") int status, @Param("companyName") String companyName, @Param("brandName") String brandName);
+    List<Brand> selectByCondition(Map map);
+
+    /**
+     * 单条件动态查询
+     * @param brand
+     * @return
+     */
+    List<Brand> selectBySingleCondition(Brand brand);
+
+    /**
+     * 添加方法
+     */
+    void add(Brand brand);
+
+    /**
+     * 修改功能
+     */
+    int update(Brand brand);
+
+    /**
+     * 删除功能
+     */
+    void deleteById(int id);
+
+    /**
+     * 批量删除
+     */
+    void deleteByIds(@Param("ids") int[] ids);
 }
--- src/test/java/MybatisTest.java
+++ src/test/java/MybatisTest.java
@@ -8,7 +8,9 @@ import pojo.Brand;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MybatisTest {
     @Test
@@ -52,6 +54,11 @@ public class MybatisTest {
         companyName = "%" + companyName + "%";
         brandName = "%" + brandName + "%";
 
+        Map map = new HashMap<>();
+        //map.put("status", status);
+        map.put("companyName", companyName);
+        //map.put("brandName", brandName);
+
         //1.加载mybatis核心配置文件，获取SqlSessionFactory
         String resource = "mybatis-config.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
@@ -59,8 +66,123 @@ public class MybatisTest {
 
         SqlSession sqlSession = sqlSessionFactory.openSession();
         BrandMapper mapper = sqlSession.getMapper(BrandMapper.class);
-        List<Brand> brands = mapper.selectByCondition(status, companyName, brandName);
+//        List<Brand> brands = mapper.selectByCondition(status, companyName, brandName);
+        List<Brand> brands = mapper.selectByCondition(map);
         System.out.println(brands);
+
+        sqlSession.close();
+    }
+
+    @Test
+    public void testSelectBySingleCondition() throws IOException {
+        int status = 1;
+        String companyName = "华为";
+        String brandName = "华为";
+
+        //处理模糊查询
+        companyName = "%" + companyName + "%";
+        brandName = "%" + brandName + "%";
+
+        Brand brand = new Brand();
+        brand.setStatus(status);
+        //brand.setCompanyName(companyName);
+        //brand.setBrandName(brandName);
+
+
+        //1.加载mybatis核心配置文件，获取SqlSessionFactory
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        BrandMapper mapper = sqlSession.getMapper(BrandMapper.class);
+        List<Brand> brands = mapper.selectBySingleCondition(brand);
+        System.out.println(brands);
+
+        sqlSession.close();
+    }
+
+    @Test
+    public void testAdd() throws IOException {
+        int status = 1;
+        String companyName = "苹果";
+        String brandName = "苹果公司";
+        String description = "nb666";
+        int ordered = 1000;
+
+        Brand brand = new Brand();
+        brand.setStatus(status);
+        brand.setCompanyName(companyName);
+        brand.setBrandName(brandName);
+        brand.setDescription(description);
+        brand.setOrdered(ordered);
+
+
+        //1.加载mybatis核心配置文件，获取SqlSessionFactory
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        //设置事务关闭
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+        BrandMapper mapper = sqlSession.getMapper(BrandMapper.class);
+        mapper.add(brand);
+
+        //提交事务
+        sqlSession.commit();
+
+        sqlSession.close();
+    }
+
+    @Test
+    public void testUpdate() throws IOException {
+        int status = 1;
+        String companyName = "苹果";
+        String brandName = "苹果公司";
+        String description = "nb777";
+        int ordered = 10000;
+        int id = 6;
+
+        Brand brand = new Brand();
+        brand.setStatus(status);
+        brand.setCompanyName(companyName);
+        brand.setBrandName(brandName);
+        brand.setDescription(description);
+        brand.setOrdered(ordered);
+        brand.setId(id);
+
+
+        //1.加载mybatis核心配置文件，获取SqlSessionFactory
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+        BrandMapper mapper = sqlSession.getMapper(BrandMapper.class);
+        int update = mapper.update(brand);
+        System.out.println(update);
+
+        //提交事务
+        sqlSession.commit();
+
+        sqlSession.close();
+    }
+
+    @Test
+    public void testDeleteById() throws IOException {
+        int id = 6;
+
+        Brand brand = new Brand();
+        brand.setId(id);
+
+        //1.加载mybatis核心配置文件，获取SqlSessionFactory
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+        BrandMapper mapper = sqlSession.getMapper(BrandMapper.class);
+        mapper.deleteById(id);
 
         sqlSession.close();
     }
